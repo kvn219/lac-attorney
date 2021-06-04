73b781fd4caf960918649ba29b30c4507fa2d27d
Kevin Nguyen <kvn219@nyu.edu>
2021-06-04 13:59:47 -0700

    save latest

--- salaries.py
+++ salaries.py
@@ -8,40 +8,31 @@ load_dotenv()
 
 
 def salaries(data):
-
     df = data[data['DEPARTMENT_TITLE'] == 'CITY ATTORNEY']
 
     df['GROUPING'] = df['JOB_TITLE'].str.strip().replace(JOB_TITLE_MAPPING_01)
 
-    cols = ["RECORD_NBR", "PAY_YEAR", "GENDER",
-            "ETHNICITY", 'GROUPING', 'EMPLOYMENT_TYPE']
-
+    cols = ['RECORD_NBR', 'PAY_YEAR', 'GENDER', 'ETHNICITY', 'GROUPING', 'EMPLOYMENT_TYPE']
     df2 = df.groupby(cols)["TOTAL_PAY"].sum().reset_index()
 
-    cols2 = ['RECORD_NBR', 'PAY_YEAR', 'GENDER', 'ETHNICITY',
-             'JOB_TITLE', 'GROUPING', 'EMPLOYMENT_TYPE']
+    cols2 = ['RECORD_NBR', 'PAY_YEAR', 'GENDER', 'ETHNICITY', 'JOB_TITLE', 'GROUPING', 'EMPLOYMENT_TYPE']
 
     df3 = df[cols2].fillna('')
 
-    df3['JOB_TITLE'] = df3.groupby(['RECORD_NBR', 'PAY_YEAR', 'GENDER', 'ETHNICITY', 'GROUPING', 'EMPLOYMENT_TYPE'])[
-        'JOB_TITLE'].transform(lambda x: ';'.join(x))
+    df3['JOB_TITLE'] = df3.groupby(cols)['JOB_TITLE'].transform(lambda x: ';'.join(x))
 
-    salaries = pd.merge(df2, df3, on=[
-                        'RECORD_NBR', 'PAY_YEAR', 'GENDER', 'ETHNICITY', 'GROUPING', 'EMPLOYMENT_TYPE'])
+    salaries = pd.merge(df2, df3, on=cols)
     salaries = salaries.drop_duplicates()
 
     df['JOB_TITLE_II'] = df['JOB_TITLE'].str.strip().replace(JOB_TITLE_MAPPING_02)
-    cols_foo = ['RECORD_NBR', 'PAY_YEAR',
-                'GENDER', 'ETHNICITY', 'JOB_TITLE_II']
+    cols_foo = ['RECORD_NBR', 'PAY_YEAR', 'GENDER', 'ETHNICITY', 'JOB_TITLE_II']
     df4 = df[cols_foo]
     df4 = df4.drop_duplicates()
 
-    salaries = pd.merge(salaries, df4, on=[
-                        'RECORD_NBR', 'PAY_YEAR', 'GENDER', 'ETHNICITY'])
+    salaries = pd.merge(salaries, df4, on=['RECORD_NBR', 'PAY_YEAR', 'GENDER', 'ETHNICITY'])
     salaries = salaries.drop_duplicates()
 
-    salaries['ETHNICITY'] = salaries['ETHNICITY'].str.strip().replace(
-        ETHNICITY_MAPPING)
+    salaries['ETHNICITY'] = salaries['ETHNICITY'].str.strip().replace(ETHNICITY_MAPPING)
 
     salaries['PAY_YEAR'] = salaries['PAY_YEAR'].apply(lambda x: f"{x}0101")
 
